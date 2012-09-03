c1a9318fd624cfc3f670b15d3e53bdf8b604f45e
Eric Lemes <ericlemes@gmail.com>
2012-09-03 20:12:04 -0300

    First version of another Tcp client and server implementation

---
+++ IntegrationTests.TestClasses/Client/TcpClientMultiThreadTest2.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Build.Utilities;
+using System.IO;
+using System.Net.Sockets;
+using IntegrationTests.ServiceClasses;
+using System.Diagnostics;
+using System.Threading;
+using Microsoft.Build.Framework;
+
+namespace IntegrationTests.TestClasses.Client
+{
+	public class TcpClientMultiThreadTest2 : Task
+	{
+
+		[Required]
+		public int TotalBatches
+		{
+			get;
+			set;
+		}
+
+		[Required]
+		public int BatchSize
+		{
+			get;
+			set;
+		}
+
+		[Required]
+		public string ConnString
+		{
+			get;
+			set;
+		}
+
+		[Required]
+		public string HostName
+		{
+			get;
+			set;
+		}
+
+		[Required]
+		public int Port
+		{
+			get;
+			set;
+		}
+
+		private Queue<MemoryStream> memoryStreamQueue = new Queue<MemoryStream>();
+
+		private StreamUtil util = new StreamUtil();		
+
+		private int batchesSent = 0;
+
+		private int recordCount = 1;
+
+		private ManualResetEvent sendDone = new ManualResetEvent(false);
+		private ManualResetEvent receiveDone = new ManualResetEvent(false);
+
+		public override bool Execute()
+		{
+			Stopwatch watch = new Stopwatch();
+			watch.Start();			
+
+			Log.LogMessage("Starting TCP transfer (multi thread) with " + TotalBatches.ToString() + " batchs with " + BatchSize.ToString() + " items each");
+
+			TcpClient tcpClient = new TcpClient();
+			tcpClient.Connect(HostName, Port);
+			NetworkStream stream = tcpClient.GetStream();			
+
+			WriteHeader(stream);
+			ReadHeader(stream);
+
+			sendDone.WaitOne();
+			receiveDone.WaitOne();
+
+			tcpClient.Close();
+			watch.Stop();			
+
+			Log.LogMessage("Total processing time: " + watch.Elapsed.TotalSeconds.ToString("0.00") + " seconds");
+
+			return true;
+		}
+
+		private void WriteHeader(NetworkStream stream)
+		{
+			batchesSent++;
+
+			if (batchesSent > TotalBatches)
+			{
+				//Mark end of transfer.
+				byte[] b = new byte[sizeof(long)];
+				b = BitConverter.GetBytes((long)0);
+				stream.Write(b, 0, sizeof(long));
+				sendDone.Set();
+				return;
+			}
+
+			Log.LogMessage("Sending request " + batchesSent.ToString());
+
+			TcpClient2OutputStreamContext ctx = new TcpClient2OutputStreamContext();
+			ctx.OutputStream = new MemoryStream();
+			ctx.ClientStream = stream;
+			util.GenerateBigRequest(ctx.OutputStream, false, recordCount, recordCount + (BatchSize - 1));
+			ctx.OutputStream.Seek(0, SeekOrigin.Begin);			
+
+			byte[] header = BitConverter.GetBytes(ctx.OutputStream.Length);
+			stream.BeginWrite(header, 0, header.Length, BeginWriteCallback, ctx);			
+
+			recordCount += BatchSize;			
+		}
+
+		private void BeginWriteCallback(IAsyncResult result)
+		{
+			TcpClient2OutputStreamContext ctx = (TcpClient2OutputStreamContext)result.AsyncState;
+			ctx.ClientStream.EndWrite(result);			
+
+			if (ctx.OutputStream.Position < ctx.OutputStream.Length)
+			{
+				byte[] buffer = new byte[256];
+				int read = ctx.OutputStream.Read(buffer, 0, buffer.Length);
+				ctx.ClientStream.BeginWrite(buffer, 0, read, BeginWriteCallback, ctx);
+			}
+			else
+			{
+				WriteHeader(ctx.ClientStream);
+			}
+		}
+
+		private int contextCount = 0;
+
+		private void ReadHeader(NetworkStream stream)
+		{
+			contextCount++;
+			TcpClient2InputStreamContext ctx = new TcpClient2InputStreamContext();
+			ctx.ID = contextCount;
+			ctx.ClientStream = stream;
+			ctx.Header = new byte[sizeof(long)];
+			ctx.HeaderRead = false;
+			
+			stream.BeginRead(ctx.Header, 0, ctx.Header.Length, BeginReadCallback, ctx);
+		}
+
+		private void BeginReadCallback(IAsyncResult result)
+		{
+			TcpClient2InputStreamContext ctx = (TcpClient2InputStreamContext)result.AsyncState;
+			int bytesRead = ctx.ClientStream.EndRead(result);
+			Log.LogMessage("Read " + bytesRead.ToString() + " bytes");
+			
+			if (!ctx.HeaderRead)
+			{
+				Log.LogMessage("Reading Header");
+				ctx.ResponseSize = BitConverter.ToInt64(ctx.Header, 0);
+				ctx.TotalRead = 0;
+				Log.LogMessage("Response Size: " + ctx.ResponseSize.ToString());
+				ctx.Header = null; //I don't need this buffer anymore. 
+				ctx.HeaderRead = true;
+				ctx.InputStream = new MemoryStream();
+				ctx.InputStream.SetLength(ctx.ResponseSize);
+
+				if (ctx.ResponseSize > 0)
+				{
+					ctx.Buffer = new byte[256];
+					int bytesToRead = (ctx.ResponseSize - ctx.TotalRead) > ctx.Buffer.Length ? ctx.Buffer.Length : (int)(ctx.ResponseSize - ctx.TotalRead);
+					Log.LogMessage("Reading " + bytesToRead.ToString() + " bytes");
+					ctx.ClientStream.BeginRead(ctx.Buffer, 0, bytesToRead, BeginReadCallback, ctx);
+				}
+				else
+					receiveDone.Set();
+			}
+			else
+			{
+				ctx.TotalRead += bytesRead;
+				Log.LogMessage("Writing " + bytesRead.ToString() + " to input stream");
+				ctx.InputStream.Write(ctx.Buffer, 0, bytesRead);				
+
+				if (ctx.TotalRead < ctx.ResponseSize)					
+				{					
+					int bytesToRead = (ctx.ResponseSize - ctx.TotalRead) > ctx.Buffer.Length ? ctx.Buffer.Length : (int)(ctx.ResponseSize - ctx.TotalRead);
+					Log.LogMessage("Reading " + bytesToRead.ToString() + " bytes");
+					ctx.Buffer = new byte[256];
+					ctx.ClientStream.BeginRead(ctx.Buffer, 0, bytesToRead, BeginReadCallback, ctx);
+				}
+				else
+				{
+					Log.LogMessage("Finished reading " + ctx.InputStream.Length.ToString() + " bytes");
+					ctx.InputStream.Seek(0, SeekOrigin.Begin);
+					Log.LogMessage("Before import stream " + ctx.ID.ToString());
+					util.ImportarStream(ConnString, ctx.InputStream);
+					Log.LogMessage("Stream imported " + ctx.ID.ToString());
+
+					ReadHeader(ctx.ClientStream);
+				}
+			}														
+		}
+	}
+
+	internal class TcpClient2InputStreamContext
+	{
+		public int ID
+		{
+			get;
+			set;
+		}
+
+		public NetworkStream ClientStream
+		{
+			get;
+			set;
+		}
+
+		public byte[] Header
+		{
+			get;
+			set;
+		}
+
+		public byte[] Buffer
+		{
+			get;
+			set;
+		}
+
+		public long ResponseSize
+		{
+			get;
+			set;
+		}
+
+		public MemoryStream InputStream
+		{
+			get;
+			set;
+		}
+
+		public bool HeaderRead
+		{
+			get;
+			set;
+		}
+
+		public long TotalRead
+		{
+			get;
+			set;
+		}		
+	}
+
+	internal class TcpClient2OutputStreamContext
+	{
+		public MemoryStream OutputStream
+		{
+			get;
+			set;
+		}
+
+		public NetworkStream ClientStream
+		{
+			get;
+			set;
+		}
+	}
+}
---
+++ IntegrationTests.TestClasses/Server/TcpTestServer2.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Build.Utilities;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.Build.Framework;
+using IntegrationTests.ServiceClasses;
+
+namespace IntegrationTests.TestClasses.Server
+{
+	public class TcpTestServer2 : Task
+	{
+
+		[Required]
+		public string ConnString
+		{
+			get;
+			set;
+		}
+
+		private int port = 8081;
+		public int Port
+		{
+			get { return port; }
+			set { port = value; }
+		}				
+
+		private StreamUtil streamUtil;
+
+		private int readCount = 0;
+		private int writeCount = 0;		
+
+		public override bool Execute()
+		{
+			Log.LogMessage("Listening to TCP Connections on port " + Port.ToString());
+
+			IPAddress ipAddress = IPAddress.Any;
+			TcpListener tcpListener = new TcpListener(ipAddress, Port);
+			tcpListener.Start();
+			tcpListener.BeginAcceptTcpClient(BeginAcceptSocketCallback, tcpListener);			
+
+			while (true)
+				Thread.Sleep(250);
+		}
+
+		private void BeginAcceptSocketCallback(IAsyncResult result)
+		{
+			Log.LogMessage("Received socket connection");
+
+			streamUtil = new StreamUtil();
+
+			TcpListener tcpListener = (TcpListener)result.AsyncState;
+			TcpClient tcpClient = tcpListener.EndAcceptTcpClient(result);			
+			NetworkStream clientStream = tcpClient.GetStream();
+
+			TcpServer2ConnectionContext ctx = new TcpServer2ConnectionContext();
+			ctx.FirstResponse = true;
+			ctx.ClientStream = clientStream;
+
+			BeginRead(ctx);
+
+			tcpListener.BeginAcceptTcpClient(BeginAcceptSocketCallback, tcpListener);
+		}
+
+		private void BeginRead(TcpServer2ConnectionContext connectionContext)
+		{
+			TcpServer2InputStreamContext ctx = new TcpServer2InputStreamContext();
+			ctx.RequestStream = new MemoryStream();
+			ctx.ConnectionContext = connectionContext;
+			ctx.ClientStream = connectionContext.ClientStream;
+			ctx.Header = new byte[sizeof(long)];
+			ctx.ClientStream.BeginRead(ctx.Header, 0, sizeof(long), BeginReadCallback, ctx);					
+		}
+
+		private void BeginReadCallback(IAsyncResult result)
+		{			
+			TcpServer2InputStreamContext ctx = result.AsyncState as TcpServer2InputStreamContext;
+			int bytesRead = ctx.ClientStream.EndRead(result);			
+
+			if (!ctx.HeaderRead)
+			{
+				readCount++;
+				Log.LogMessage("Reading " + readCount.ToString());
+				ctx.HeaderRead = true;
+				ctx.RemainingBytes = BitConverter.ToInt64(ctx.Header, 0);
+			}
+			else
+			{
+				ctx.RemainingBytes -= bytesRead;
+				ctx.RequestStream.Write(ctx.Buffer, 0, bytesRead);
+				if (ctx.RemainingBytes == 0)
+					ctx.FinishedReading = true;
+			}
+
+			if (ctx.RemainingBytes <= 0)
+			{
+				if (ctx.FinishedReading)
+				{
+					TcpServer2OutputStreamContext outputContext = new TcpServer2OutputStreamContext();
+					outputContext.ConnectionContext = ctx.ConnectionContext;
+					outputContext.OutputStream = new MemoryStream();
+					
+					ctx.RequestStream.Seek(0, SeekOrigin.Begin);					
+					
+					streamUtil.ProcessClientBigRequest(ConnString, ctx.RequestStream, outputContext.OutputStream, false, null);					
+
+					outputContext.OutputStream.Seek(0, SeekOrigin.Begin);
+					outputContext.ClientStream = ctx.ClientStream;
+
+					byte[] buffer = BitConverter.GetBytes(outputContext.OutputStream.Length);
+
+					writeCount++;
+					Log.LogMessage("Finished reading " + readCount.ToString());
+					Log.LogMessage("Writing " + writeCount.ToString());
+
+					BeginRead(ctx.ConnectionContext);
+
+					if (ctx.ConnectionContext.FirstResponse)
+					{
+						ctx.ConnectionContext.FirstResponse = false;
+						outputContext.ClientStream.BeginWrite(buffer, 0, buffer.Length, BeginWriteCallback, outputContext);
+					}
+					else
+					{
+						ctx.ConnectionContext.OutputQueue.Enqueue(outputContext);
+					}
+				}
+				else
+				{
+					Log.LogMessage("Writing empty header");
+					TcpServer2OutputStreamContext outputContext = new TcpServer2OutputStreamContext();
+					outputContext.ConnectionContext = ctx.ConnectionContext;
+					outputContext.EmptyResponse = true;
+					outputContext.ClientStream = ctx.ClientStream;
+					ctx.ConnectionContext.OutputQueue.Enqueue(outputContext);					
+				}
+			}
+			else
+			{
+				ctx.Buffer = new byte[256];
+				int bytesToRead = ctx.RemainingBytes > ctx.Buffer.Length ? ctx.Buffer.Length : (int)ctx.RemainingBytes;								
+				ctx.ClientStream.BeginRead(ctx.Buffer, 0, bytesToRead, BeginReadCallback, ctx);
+			}
+		}
+
+		private void BeginWriteCallback(IAsyncResult result)
+		{
+			TcpServer2OutputStreamContext ctx = result.AsyncState as TcpServer2OutputStreamContext;
+			ctx.ClientStream.EndWrite(result);
+
+			if (ctx.EmptyResponse)
+				return;
+
+			byte[] buffer = new byte[256];
+			int bytesWritten = ctx.OutputStream.Read(buffer, 0, buffer.Length);
+
+			if (bytesWritten > 0)
+			{
+				ctx.ClientStream.BeginWrite(buffer, 0, bytesWritten, BeginWriteCallback, ctx);
+			}
+			else
+			{
+				Log.LogMessage("Finished writing " + writeCount.ToString());
+				TcpServer2OutputStreamContext ctx2 = ctx.ConnectionContext.OutputQueue.Dequeue();
+				if (ctx2.EmptyResponse)
+				{
+					buffer = BitConverter.GetBytes((long)0);
+					ctx2.ClientStream.BeginWrite(buffer, 0, buffer.Length, BeginWriteCallback, ctx2);
+				}
+				else
+				{
+					buffer = BitConverter.GetBytes(ctx2.OutputStream.Length);
+					ctx2.ClientStream.BeginWrite(buffer, 0, buffer.Length, BeginWriteCallback, ctx2);
+				}
+			}
+		}
+	}
+
+	internal class TcpServer2OutputStreamContext
+	{
+		public MemoryStream OutputStream
+		{
+			get;
+			set;
+		}
+
+		public NetworkStream ClientStream
+		{
+			get;
+			set;
+		}
+
+		public bool EmptyResponse
+		{
+			get;
+			set;
+		}
+
+		public TcpServer2ConnectionContext ConnectionContext
+		{
+			get;
+			set;
+		}
+	}
+
+	internal class TcpServer2InputStreamContext
+	{
+		public byte[] Header
+		{
+			get;
+			set;
+		}
+
+		public NetworkStream ClientStream
+		{
+			get;
+			set;
+		}
+
+		public MemoryStream RequestStream
+		{
+			get;
+			set;
+		}
+
+		public long RemainingBytes
+		{
+			get;
+			set;
+		}
+
+		public bool HeaderRead
+		{
+			get;
+			set;
+		}
+
+		public byte[] Buffer
+		{
+			get;
+			set;
+		}
+
+		public bool FinishedReading
+		{
+			get;
+			set;
+		}
+
+		public TcpServer2ConnectionContext ConnectionContext
+		{
+			get;
+			set;
+		}
+	}
+
+	internal class TcpServer2ConnectionContext
+	{
+		public bool FirstResponse
+		{
+			get;
+			set;
+		}
+
+		public NetworkStream ClientStream
+		{
+			get;
+			set;
+		}
+
+		private Queue<TcpServer2OutputStreamContext> outputQueue = new Queue<TcpServer2OutputStreamContext>();
+		public Queue<TcpServer2OutputStreamContext> OutputQueue
+		{
+			get { return outputQueue; }			
+		}
+	}
+}
