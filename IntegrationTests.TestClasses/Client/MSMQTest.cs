e2c817d6880947ecada1b53ef4e59da35b79b23a
Eric Lemes <ericlemes@gmail.com>
2012-12-27 21:13:05 -0200

    Improving MSMQ implementation

--- IntegrationTests.TestClasses/Client/MSMQTest.cs
+++ IntegrationTests.TestClasses/Client/MSMQTest.cs
@@ -9,99 +9,143 @@ using Microsoft.Build.Utilities;
 using Microsoft.Build.Framework;
 using System.Threading;
 using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace IntegrationTests.TestClasses.Client
 {
-    public class MSMQTest : Task
-    {
-        [Required]
-        public string InputQueueName
-        {
-            get;
-            set;
-        }
+	public class MSMQTest : Microsoft.Build.Utilities.Task
+	{
+		[Required]
+		public string InputQueueName
+		{
+			get;
+			set;
+		}
 
-        [Required]
-        public string OutputQueueName
-        {
-            get;
-            set;
-        }
+		[Required]
+		public string OutputQueueName
+		{
+			get;
+			set;
+		}
 
-        [Required]
-        public int TotalBatches
-        {
-            get;
-            set;
-        }
+		[Required]
+		public int TotalBatches
+		{
+			get;
+			set;
+		}
 
-        [Required]
-        public int BatchSize
+		[Required]
+		public int BatchSize
+		{
+			get;
+			set;
+		}
 
-        {
-            get;
-            set;
-        }
+		[Required]
+		public string ConnString
+		{
+			get;
+			set;
+		}
 
-        public string ConnString
-        {
-            get;
-            set;
-        }        
+		[Required]
+		public bool IsTransactional
+		{
+			get;
+			set;
+		}
 
-        private MessageQueue inputQueue;
+		private MessageQueue inputQueue;
 
-        private int messageCount;
-        private bool finished = false;
+		private int messageCount;		
 
-        public override bool Execute()
-        {
-            Stopwatch watch = new Stopwatch();
-            watch.Start();
+		private ManualResetEvent processingDone = new ManualResetEvent(false);
 
-            Log.LogMessage("Starting MSMQ transfer with " + TotalBatches.ToString() + " batchs with " + BatchSize.ToString() + " items each");
+		private Queue<Message> messageInputQueue = new Queue<Message>();
 
-            messageCount = TotalBatches;
+		public override bool Execute()
+		{
+			Stopwatch watch = new Stopwatch();
+			watch.Start();
 
-            inputQueue = new MessageQueue(InputQueueName);
-            inputQueue.ReceiveCompleted += new ReceiveCompletedEventHandler(inQueue_ReceiveCompleted);
-            inputQueue.BeginReceive();
+			Log.LogMessage("Starting MSMQ transfer with " + TotalBatches.ToString() + " batchs with " + BatchSize.ToString() + " items each");
 
-            MessageQueue queue = new MessageQueue(OutputQueueName);            
-            int count = 1;            
+			messageCount = TotalBatches;
 
-            for (int i = 0; i < TotalBatches; i++)
-            {
-                Message msg = new Message();
-                msg.BodyStream = new MemoryStream();
-                
-                StreamUtil.GenerateBigRequest(msg.BodyStream, false, count, count + (BatchSize - 1));
-                msg.BodyStream.Seek(0, SeekOrigin.Begin);
-                queue.Send(msg, MessageQueueTransactionType.Single);                
+			inputQueue = new MessageQueue(InputQueueName);
+			inputQueue.ReceiveCompleted += new ReceiveCompletedEventHandler(inQueue_ReceiveCompleted);			
+			inputQueue.BeginReceive();
 
-                count += BatchSize;                
-            }
+			System.Threading.Tasks.Task.Factory.StartNew(ProcessInputQueue);
 
-            while (!finished)
-                Thread.Sleep(250);
+			MessageQueue queue = new MessageQueue(OutputQueueName);
 
-            watch.Stop();
-            Log.LogMessage("Total processing time: " + watch.Elapsed.TotalSeconds.ToString("0.00") + " seconds");
+			int count = 1;
 
-            return true;
-        }
+			for (int i = 0; i < TotalBatches; i++)
+			{
+				Message msg = new Message();
+				msg.BodyStream = new MemoryStream();
 
-        private void inQueue_ReceiveCompleted(object sender, ReceiveCompletedEventArgs e)
-        {            
-            Log.LogMessage("Received message. Waiting for " + messageCount.ToString() + " messages");
+				StreamUtil.GenerateBigRequest(msg.BodyStream, false, count, count + (BatchSize - 1));
+				msg.BodyStream.Seek(0, SeekOrigin.Begin);
+				queue.Send(msg, IsTransactional ? MessageQueueTransactionType.Single : MessageQueueTransactionType.None);
 
-            messageCount--;
-            StreamUtil.ImportarStream(ConnString, e.Message.BodyStream);            
+				count += BatchSize;
+			}
+			Log.LogMessage("Messages sent");
 
-            if (messageCount <= 0)
-                finished = true;
-            else
-                inputQueue.BeginReceive();  
-        }
-    }
+			processingDone.WaitOne();			
+
+			watch.Stop();
+			Log.LogMessage("Total processing time: " + watch.Elapsed.TotalSeconds.ToString("0.00") + " seconds");
+
+			return true;
+		}
+
+		int enqueueCount = 0;
+		private void inQueue_ReceiveCompleted(object sender, ReceiveCompletedEventArgs e)
+		{		
+			
+			lock (messageInputQueue)
+			{
+				messageInputQueue.Enqueue(e.Message);
+				enqueueCount++;
+				Log.LogMessage("Enqueued " + enqueueCount.ToString() + " messages");
+			}
+		
+			if (enqueueCount < TotalBatches)
+				inputQueue.BeginReceive();
+		}
+
+		private void ProcessInputQueue()
+		{
+			while (true)
+			{
+				Message msg = null;
+
+				lock (messageInputQueue)
+				{
+					if (messageInputQueue.Count > 0)
+						msg = messageInputQueue.Dequeue();
+				}
+
+				if (msg == null)
+				{
+					Thread.Sleep(50);
+					continue;
+				}
+				
+				StreamUtil.ImportarStream(ConnString, msg.BodyStream);
+				Log.LogMessage("Processed message " + messageCount.ToString());					
+				messageCount--;					
+
+				if (messageCount <= 0)
+					break;
+			}
+			processingDone.Set();
+		}
+	}
 }
--- IntegrationTests.TestClasses/Server/MSMQTestServer.cs
+++ IntegrationTests.TestClasses/Server/MSMQTestServer.cs
@@ -12,64 +12,101 @@ using System.Diagnostics;
 
 namespace IntegrationTests.TestClasses.Server
 {
-    public class MSMQTestServer : Task
-    {
-        [Required]
-        public string OutputQueueName
-        {
-            get;
-            set;
-        }
+	public class MSMQTestServer : Task
+	{
+		[Required]
+		public string OutputQueueName
+		{
+			get;
+			set;
+		}
 
-        [Required]
-        public string InputQueueName
-        {
-            get;
-            set;
-        }
+		[Required]
+		public string InputQueueName
+		{
+			get;
+			set;
+		}
 
-        [Required]
-        public string ConnString
-        {
-            get;
-            set;
-        }
-                
-        private int messageCount = 0;                        
-        private MessageQueue inputQueue;
-        private MessageQueue outputQueue;        
+		[Required]
+		public string ConnString
+		{
+			get;
+			set;
+		}
 
-        public override bool  Execute()
-        {
-            Log.LogMessage("Starting MSMQ Server");
+		[Required]
+		public bool IsTransactional
+		{
+			get;
+			set;
+		}
 
-            outputQueue = new MessageQueue(OutputQueueName);
+		private int messageCount = 0;
+		private int responseCount = 0;
+		private MessageQueue inputQueue;
+		private MessageQueue outputQueue;
 
-            inputQueue = new MessageQueue(InputQueueName);
-            inputQueue.ReceiveCompleted += new ReceiveCompletedEventHandler(queue_ReceiveCompleted);
-            inputQueue.BeginReceive();
+		private ManualResetEvent processingDone = new ManualResetEvent(false);
 
-            while (true)
-                Thread.Sleep(250);                        
-        }
+		private Queue<Message> processingInputQueue = new Queue<Message>();
 
-        private void queue_ReceiveCompleted(object sender, ReceiveCompletedEventArgs e)
-        {
-            
-            Log.LogMessage("Message received " + messageCount.ToString());
-            messageCount++;
+		public override bool Execute()
+		{
+			Log.LogMessage("Starting MSMQ Server");
 
-            Message resp = new Message();
-            resp.BodyStream = new MemoryStream();
-            
-            StreamUtil.ProcessClientBigRequest(ConnString, e.Message.BodyStream, resp.BodyStream, false, null);
-            resp.BodyStream.Seek(0, SeekOrigin.Begin);
-            
-            outputQueue.Send(resp, MessageQueueTransactionType.Single);
+			outputQueue = new MessageQueue(OutputQueueName);
 
-            inputQueue.BeginReceive();
-        }
+			inputQueue = new MessageQueue(InputQueueName);
+			inputQueue.ReceiveCompleted += new ReceiveCompletedEventHandler(queue_ReceiveCompleted);
+			inputQueue.BeginReceive();
 
+			System.Threading.Tasks.Task.Factory.StartNew(ProcessInputQueue);
 
-    }
+			processingDone.WaitOne();
+			return true;
+		}
+
+		private void queue_ReceiveCompleted(object sender, ReceiveCompletedEventArgs e)
+		{
+			lock (processingInputQueue)
+				processingInputQueue.Enqueue(e.Message);
+
+			Log.LogMessage("Message received " + messageCount.ToString());
+			messageCount++;		
+
+			inputQueue.BeginReceive();
+		}
+
+		private void ProcessInputQueue()
+		{
+			while (true)
+			{
+				Message msg = null;
+				lock (processingInputQueue)
+				{
+					if (processingInputQueue.Count > 0)
+						msg = processingInputQueue.Dequeue();
+				}
+				if (msg == null)
+				{
+					Thread.Sleep(50);
+					continue;
+				}					
+
+				Message resp = new Message();
+				resp.BodyStream = new MemoryStream();
+
+				StreamUtil.ProcessClientBigRequest(ConnString, msg.BodyStream, resp.BodyStream, false, null);
+				resp.BodyStream.Seek(0, SeekOrigin.Begin);
+
+				responseCount++;
+
+				Log.LogMessage("Putting response " + responseCount.ToString());
+				outputQueue.Send(resp, IsTransactional ? MessageQueueTransactionType.Single : MessageQueueTransactionType.None);
+				Log.LogMessage("Response on queue" + responseCount.ToString());
+			}			
+		}
+
+	}
 }
